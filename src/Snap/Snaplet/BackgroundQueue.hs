26d4da7b845757061464f2c071e8f0fa62c73a0c
David Vollbracht <david.vollbracht@gmail.com>
2012-03-17 22:45:07 -0400

    Replace emptyMasterChan with not creating a chan at all if there are no actions

--- src/Snap/Snaplet/BackgroundQueue.hs
+++ src/Snap/Snaplet/BackgroundQueue.hs
@@ -19,26 +19,34 @@ import           Data.Lens.Lazy
 
 import           Snap.Snaplet
 
-data BackgroundQueue a = BackgroundQueue (Chan (Job a))
+type BackgroundChan a = Maybe (Chan (Job a))
+type Action a = a -> IO ()
+
+data BackgroundQueue a = BackgroundQueue (BackgroundChan a)
+
 data Job a = Job a | Quit
 
 class HasBackgroundQueue a b where
   backgroundQueueLens :: Lens (Snaplet a) (Snaplet (BackgroundQueue b))
 
-backgroundQueueInit :: [a -> IO ()] -> SnapletInit b (BackgroundQueue a)
+sendToBackground :: BackgroundChan a -> Job a -> IO ()
+sendToBackground Nothing _ = return ()
+sendToBackground (Just c) job = writeChan c job
+
+backgroundQueueInit :: [Action a] -> SnapletInit b (BackgroundQueue a)
 backgroundQueueInit actions = do
   makeSnaplet "backgroundQueue" "" Nothing $ do
-    chan <- liftIO $ newChan
-    liftIO $ forkIO $ emptyMasterChan chan
+    chan <- liftIO $ foldM forkAction Nothing actions
 
-    forM_ actions $ \action -> do
-      liftIO $ do
-        actionChan <- dupChan chan
-        forkIO $ backgroundThread actionChan action
-
-    onUnload $ writeChan chan Quit
+    onUnload $ sendToBackground chan Quit
 
     return (BackgroundQueue chan)
+
+forkAction :: BackgroundChan a -> Action a -> IO (BackgroundChan a)
+forkAction lastChan action = do
+  actionChan <- maybe newChan dupChan lastChan
+  forkIO $ backgroundThread actionChan action
+  return (Just actionChan)
 
 queueInBackground :: HasBackgroundQueue a b
                   => b
@@ -47,7 +55,7 @@ queueInBackground jobData = do
   with' backgroundQueueLens $ do
     snaplet <- getSnapletState
     let BackgroundQueue chan = getL snapletValue snaplet
-    liftIO $ writeChan chan (Job jobData)
+    liftIO $ sendToBackground chan (Job jobData)
     return ()
 
 backgroundThread :: Chan (Job a) -> (a -> IO ()) -> IO ()
@@ -66,5 +74,3 @@ runAction action = do
                   putStrLn err)
 
 
-emptyMasterChan :: Chan a -> IO ()
-emptyMasterChan chan = readChan chan >> emptyMasterChan chan
