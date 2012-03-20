65fb1df4925dc3671ee9f90f39c36e09457dc69d
David Vollbracht <david@flipstone.com>
2012-03-20 18:40:06 -0400

    Started

--- src/Snap/Snaplet/BackgroundQueue.hs
+++ src/Snap/Snaplet/BackgroundQueue.hs
@@ -1,4 +1,7 @@
-{-# LANGUAGE OverloadedStrings, MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Snap.Snaplet.BackgroundQueue
   ( BackgroundQueue
@@ -8,8 +11,9 @@ module Snap.Snaplet.BackgroundQueue
   , queueInBackground
   ) where
 
-import           Prelude hiding (catch)
+import           Prelude hiding (catch, (.))
 
+import qualified Control.Category as Category
 import           Control.Concurrent
 import           Control.Exception (catch, SomeException)
 import           Control.Monad
@@ -18,23 +22,61 @@ import           Control.Monad.Trans
 
 import           Data.Lens.Lazy
 
-import           Snap.Snaplet
+import           Snap.Snaplet hiding (Handler)
+import qualified Snap.Snaplet as Snaplet
 
-type BackgroundChan a = Maybe (Chan (Job a))
-type Action a = a -> IO ()
+data Job a b = Job a (Lens (Snaplet b) (Snaplet b)) (Snaplet b)
+             | Quit
+type BackgroundChan a b = Maybe (Chan (Job a b))
+type Action b a = a -> Handler b b ()
 
-data BackgroundQueue a = BackgroundQueue (BackgroundChan a)
+data LensedIO b v a = LensedIO {
+   lensedAction :: Lens b v
+                -> b
+                -> v
+                -> IO (b, v, a)
+  }
 
-data Job a = Job a | Quit
+newtype Handler b v a =
+   Handler (LensedIO (Snaplet b) (Snaplet v) a)
+ deriving (Monad, MonadState (Snaplet v))
+
+instance Monad (LensedIO b v) where
+  return a = LensedIO $ \_ b v -> return (b, v, a)
+  (LensedIO h) >>= f = LensedIO $ \l b v -> do
+                                    (b', v', a) <- h l b v
+                                    let LensedIO h' = f a
+                                    h' l b' v'
+
+instance MonadState v (LensedIO b v) where
+  get = LensedIO $ \_ b v -> return (b, v, v)
+  put v' = LensedIO $ \_ b _ -> return (b, v', ())
+
+instance MonadSnaplet Handler where
+  withTop' lens (Handler lensedIO) = Handler $ LensedIO $ \l b v -> do
+    let v' = getL lens b
+        LensedIO action = lensedIO
+    (b', _, a) <- action lens b v'
+    return (b', v, a)
+
+  with' l h = do
+      l' <- getLens
+      withTop' (l `composeLens` l') h
+    where composeLens = (Category..)
+
+  getLens = Handler $ LensedIO $ \l b v -> return (b, v, l)
+  getOpaqueConfig = undefined
+
+data BackgroundQueue b a = BackgroundQueue (BackgroundChan b a)
 
 class HasBackgroundQueue a b where
-  backgroundQueueLens :: Lens (Snaplet a) (Snaplet (BackgroundQueue b))
+  backgroundQueueLens :: Lens (Snaplet a) (Snaplet (BackgroundQueue b a))
 
-writeBGChan :: BackgroundChan a -> Job a -> IO ()
+writeBGChan :: BackgroundChan a b -> Job a b -> IO ()
 writeBGChan Nothing _ = return ()
 writeBGChan (Just c) job = writeChan c job
 
-backgroundQueueInit :: [Action a] -> SnapletInit b (BackgroundQueue a)
+backgroundQueueInit :: [Action b a] -> SnapletInit b (BackgroundQueue a b)
 backgroundQueueInit actions = do
   makeSnaplet "backgroundQueue" "" Nothing $ do
     chan <- liftIO $ foldM forkAction Nothing actions
@@ -43,28 +85,36 @@ backgroundQueueInit actions = do
 
     return (BackgroundQueue chan)
 
-forkAction :: BackgroundChan a -> Action a -> IO (BackgroundChan a)
+forkAction :: BackgroundChan a b -> Action b a -> IO (BackgroundChan a b)
 forkAction lastChan action = do
   actionChan <- maybe newChan dupChan lastChan
-  forkIO $ backgroundThread actionChan action
+  forkIO $ backgroundThread actionChan $ \jobData lens snaplet -> do
+    let Handler lensed = action jobData
+    lensedAction lensed lens snaplet snaplet
+    return ()
+
   return (Just actionChan)
 
 queueInBackground :: HasBackgroundQueue a b
                   => b
-                  -> Handler a a ()
+                  -> Snaplet.Handler a a ()
 queueInBackground jobData = do
+  appLens <- getLens
+  appState <- get
   with' backgroundQueueLens $ do
     BackgroundQueue chan <- get
-    liftIO $ writeBGChan chan (Job jobData)
+    liftIO $ writeBGChan chan (Job jobData appLens appState)
     return ()
 
-backgroundThread :: Chan (Job a) -> (a -> IO ()) -> IO ()
+backgroundThread :: Chan (Job a b)
+                 -> (a -> Lens (Snaplet b) (Snaplet b) -> (Snaplet b) -> IO ())
+                 -> IO ()
 backgroundThread chan action = do
   job <- readChan chan
 
   case job of
-    (Job a) -> do runAction (action a)
-                  backgroundThread chan action
+    (Job a lens snaplet) -> do runAction (action a lens snaplet)
+                               backgroundThread chan action
     Quit -> putStrLn "Stopping background thread"
 
 runAction :: IO () -> IO ()
